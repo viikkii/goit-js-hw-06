165deacce0418cf9ac1446e1a5c526a901bf1a1b
Viktoriia Fokina <viikkii.f@gmail.com>
2021-10-21 21:48:14 +0300

    Готовое ДЗ-06

--- js/task-08.js
+++ js/task-08.js
@@ -1,9 +1,19 @@
 const form = document.querySelector('form.login-form')
 console.log(form);
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
   event.preventDefault();
-  const {
-    elements: { email, password }} = event.currentTarget;
-  console.log(email.value, password.value);
-});
+    const {
+    elements: { email, password } } = event.currentTarget;
+
+
+if (email.value === "" || password.value === "") {
+  alert `все поля должны быть заполнены!`;
+  form.reset();
+}
+
+console.log(`email: ${email.value}, password: ${password.value}`);
+event.currentTarget.reset();
+}
